ae8f726eac31c18a10f22b2e9fb43531d21c32be
Artush <art3611@gmail.com>
2025-07-20 05:09:35 +0200

    update

---
+++ invoicing/invoicing/src/main/java/entity/User.java
@@ -0,0 +1,21 @@
+package entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+}
