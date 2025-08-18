f80250a7ff47afd05ba0e7e46f200fe092bd0b7a
Artush <art3611@gmail.com>
2025-08-18 23:59:45 +0200

    update

---
+++ invoicing/invoicing/src/main/java/com/myapp/invoicing/controller/LoginViewController.java
@@ -0,0 +1,13 @@
+package com.myapp.invoicing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginViewController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login"; // Spring busca templates/login.html
+    }
+}
