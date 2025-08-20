677ddb51e1e5fb052ad043fbbc181b4c7ca72039
Artush <art3611@gmail.com>
2025-08-20 22:08:37 +0200

    update

---
+++ invoicing/invoicing/src/main/java/com/myapp/invoicing/dto/LoginRequest.java
@@ -0,0 +1,8 @@
+package com.myapp.invoicing.dto;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    // getters y setters
+}
---
+++ invoicing/invoicing/src/main/java/com/myapp/invoicing/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.myapp.invoicing.dto;
+
+
+public class LoginResponse {
+    private String token;
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
