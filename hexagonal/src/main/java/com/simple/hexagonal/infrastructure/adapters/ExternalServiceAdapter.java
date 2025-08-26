6a22825db11293c4cf037c1f6b9a86afb060f160
Artush <art3611@gmail.com>
2025-08-27 00:24:27 +0200

    update

--- hexagonal/src/main/java/com/simple/hexagonal/infrastructure/adapters/ExternalServiceAdapter.java
+++ hexagonal/src/main/java/com/simple/hexagonal/infrastructure/adapters/ExternalServiceAdapter.java
@@ -1,4 +1,75 @@
 package com.simple.hexagonal.infrastructure.adapters;
 
-public class ExternalServiceAdapter {
+import com.simple.hexagonal.domain.models.AdditionalTaskInfo;
+import com.simple.hexagonal.domain.ports.output.ExternalServicePort;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class ExternalServiceAdapter implements ExternalServicePort {
+
+    private final RestTemplate restTemplate;
+
+    public ExternalServiceAdapter(){
+        restTemplate = new RestTemplate();
+    }
+
+
+    @Override
+    public AdditionalTaskInfo getAdditionalTaskInfo(Long id) {
+        String apiUrl = "";
+        ResponseEntity<JsonPlaceHolderTodo> response = restTemplate.getForEntity(apiUrl,JsonPlaceHolderTodo.class);
+        JsonPlaceHolderTodo todo = response.getBody();
+        if()
+    }
+
+    private static class JsonPlaceHolderTodo{
+        private Long id;
+        private Long userId;
+
+        public Long getId(){
+            return id;
+        }
+
+        public void setId(Long id){
+            this.id = id;
+        }
+
+        public Long setUserId(){
+            return userId;
+        }
+        public void setUserId(Long userId){
+            this.userId = userId;
+        }
+    }
+
+    private static class JsonHolderUser{
+        private Long id;
+        private String name;
+        private String email;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
 }
--- hexagonal/src/main/java/com/simple/hexagonal/infrastructure/config/ApplicationConfig.java
+++ hexagonal/src/main/java/com/simple/hexagonal/infrastructure/config/ApplicationConfig.java
@@ -1,4 +1,42 @@
 package com.simple.hexagonal.infrastructure.config;
 
+import com.simple.hexagonal.application.services.TaskService;
+import com.simple.hexagonal.application.usecases.*;
+import com.simple.hexagonal.domain.ports.input.GetAdditionalTaskUseCase;
+import com.simple.hexagonal.domain.ports.output.ExternalServicePort;
+import com.simple.hexagonal.domain.ports.output.TaskRepositoryPort;
+import com.simple.hexagonal.infrastructure.adapters.ExternalServiceAdapter;
+import com.simple.hexagonal.infrastructure.repositories.JpaTaskRepository;
+import com.simple.hexagonal.infrastructure.repositories.JpaTaskRepositoryAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public TaskService taskService(TaskRepositoryPort taskRepositoryPort, GetAdditionalTaskUseCase getAdditionalTaskUseCase){
+        return new TaskService(
+                new CreateTaskUseCaseImp(taskRepositoryPort),
+                new RetrieveTaskUseCaseImp(taskRepositoryPort),
+                new UpdateTaskUseCaseImp(taskRepositoryPort),
+                new DeleteTaskUseCaseImp(taskRepositoryPort),
+                getAdditionalTaskUseCase
+        );
+    }
+
+    @Bean
+    public TaskRepositoryPort taskRepositoryPort(JpaTaskRepositoryAdapter jpaTaskRepositoryAdapter){
+        return jpaTaskRepositoryAdapter;
+    }
+
+    @Bean
+    public GetAdditionalTaskUseCase getAdditionalTaskUseCase(ExternalServicePort externalServicePort){
+        return new GetAdditionalTaskUseCaseImp(externalServicePort);
+    }
+
+    @Bean
+    public ExternalServicePort externalServicePort(){
+        return new ExternalServiceAdapter();
+    }
 }
